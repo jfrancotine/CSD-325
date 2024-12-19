4a4619ca58fdfbbe76a2c22c6598ed2779a9b5a6
Jose Franco <jofranco@my365.bellevue.edu>
2024-12-18 18:32:59 -0600

    Assignment 12.2 Redo Assignment

---
+++ Module-12/francoguitodo.py
@@ -0,0 +1,136 @@
+#Jose Franco
+#12/10/2024
+#Working with Tkinter to build a GUI window to simulate a To Do List.
+
+
+import tkinter as tk
+import tkinter.messagebox as msg
+
+
+#This class defines a simple ToDo application using Tkinter.
+#Users can add tasks, remove them by right-clicking, and exit the application using the File menu.
+class Todo(tk.Tk):
+    def __init__(self, tasks=None):
+        super().__init__()
+
+        if not tasks:
+            self.tasks = []
+        else:
+            self.tasks = tasks
+
+        self.tasks_canvas = tk.Canvas(self)
+
+        self.tasks_frame = tk.Frame(self.tasks_canvas)
+        self.text_frame = tk.Frame(self)
+
+        self.scrollbar = tk.Scrollbar(self.tasks_canvas, orient="vertical", command=self.tasks_canvas.yview)
+
+        self.tasks_canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.title("Franco - ToDo")
+        self.geometry("400x500")
+
+        # Menu bar
+        self.menu_bar = tk.Menu(self)
+        self.config(menu=self.menu_bar)
+
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="Exit", command=self.exit_program)
+
+        self.task_create = tk.Text(self.text_frame, height=3, bg="white", fg="black")
+
+        self.tasks_canvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas_frame = self.tasks_canvas.create_window((0,0), window=self.tasks_frame, anchor="n")
+
+        self.task_create.pack(side=tk.BOTTOM, fill=tk.X)
+        self.text_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.task_create.focus_set()
+
+        todo1 = tk.Label(self.tasks_frame, text="-- Items Added -- ** Use Right Click to Delete an Item **",
+                         bg="Purple", fg="Yellow", pady=10)
+
+        self.tasks.append(todo1)
+
+        for task in self.tasks:
+            task.pack(side=tk.TOP, fill=tk.X)
+
+        self.bind("<Return>", self.add_task)
+        self.bind("<Configure>", self.on_frame_configure)
+        self.bind_all("<MouseWheel>", self.mouse_scroll)
+        self.bind_all("<Button-4>", self.mouse_scroll)
+        self.bind_all("<Button-5>", self.mouse_scroll)
+        self.tasks_canvas.bind("<Configure>", self.task_width)
+
+        self.colour_schemes = [{"bg": "purple", "fg": "Yellow"}, {"bg": "Yellow", "fg": "purple"}]
+
+    def add_task(self, event=None):
+        #Adds a new task to the list.
+        task_text = self.task_create.get(1.0, tk.END).strip()
+
+        if len(task_text) > 0:
+            new_task = tk.Label(self.tasks_frame, text=task_text, pady=10)
+
+            self.set_task_colour(len(self.tasks), new_task)
+#Correction Button 3 instead of 2.
+            new_task.bind("<Button-3>", self.remove_task)
+            new_task.pack(side=tk.TOP, fill=tk.X)
+
+            self.tasks.append(new_task)
+
+        self.task_create.delete(1.0, tk.END)
+
+    def remove_task(self, event):
+        #Removes the task when right-clicked and confirmed by the user.
+        task = event.widget
+        if msg.askyesno("Really Delete?", "Delete " + task.cget("text") + "?"):
+            self.tasks.remove(event.widget)
+            event.widget.destroy()
+            self.recolour_tasks()
+
+    def recolour_tasks(self):
+        #Updates the color scheme of tasks after any modification.
+        for index, task in enumerate(self.tasks):
+            self.set_task_colour(index, task)
+
+    def set_task_colour(self, position, task):
+        #Sets alternating colors for tasks based on their position.
+        _, task_style_choice = divmod(position, 2)
+
+        my_scheme_choice = self.colour_schemes[task_style_choice]
+
+        task.configure(bg=my_scheme_choice["bg"])
+        task.configure(fg=my_scheme_choice["fg"])
+
+    def on_frame_configure(self, event=None):
+        # Adjusts the scrollable area whenever the frame is resized.
+        self.tasks_canvas.configure(scrollregion=self.tasks_canvas.bbox("all"))
+
+    def task_width(self, event):
+        #Ensures tasks stretch to fit the canvas width.
+        canvas_width = event.width
+        self.tasks_canvas.itemconfig(self.canvas_frame, width=canvas_width)
+
+    def mouse_scroll(self, event):
+        #Handles scrolling through the task list using the mouse.
+        if event.delta:
+            self.tasks_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+        else:
+            if event.num == 5:
+                move = 1
+            else:
+                move = -1
+
+            self.tasks_canvas.yview_scroll(move, "units")
+
+    def exit_program(self):
+        # Prompts the user to confirm exit and closes the application if confirmed.
+        if msg.askyesno("Exit", "Are you sure you want to exit?"):
+            self.destroy()
+
+
+if __name__ == "__main__":
+    todo = Todo()
+    todo.mainloop()
